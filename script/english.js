6b7af0417845b76c9a2e67be9c3bd7ff1fdee9fc
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2022-03-09 13:33:38 -0300

    Section de profile desenvolvida

--- script/english.js
+++ script/english.js
@@ -1,10 +1,26 @@
 const english = {
     'switch-description': 'Change language here',
     
-    'nav-about': 'About me',
+    'nav-profile': 'Profile',
     'nav-portfolio': 'Portfolio',
     'nav-skills' : 'Skills',
 
     'footer-contact' : 'Contact',
-    'footer-socialmedia' : "Social media"
+    'footer-socialmedia' : "Social media",
+
+    'header-profile' : 'Profile',
+    'header-desc' : 'Java Developer',
+
+    'profile-about-key': 'About me',
+    'profile-about-value' : "I am currently a PHP developer and I do consultancy for SKY (A company whose main product is pay-TV channels.) in the development of interactive applications (Applications that are transmitted via satellite along with the customer's channel package, these applications can be of pay-per-view sales, marketing of some new packages, etc.).<br><br>I was an intern at SKY for 2 years where I developed myself on the front-end and developed several automation tools in Java, and currently I am a developer at Esferas Software which has SKY as the main client.<br><br>I've been developing in Java for the last 3 years, I've always been passionate about the tool and I'm currently looking for an opportunity to get back into Java development.",
+
+    'profile-name-key' : 'Name: ',
+    'profile-name-value' : 'Leonardo Pinheiro Lacerda',
+
+    'profile-age-key' : 'Age: ',
+    'profile-age-value' : '23 Years old',
+
+    'profile-location-key' : 'Location',
+    'profile-location-value' : 'São Paulo, SP - Brazil'
+
 };
--- script/portuguese.js
+++ script/portuguese.js
@@ -1,10 +1,25 @@
 const portuguese = {
     'switch-description': 'Troque o idioma aqui',
 
-    'nav-about': 'Sobre mim',
+    'nav-profile': 'Perfil',
     'nav-portfolio': 'Portfólio',
     'nav-skills' : 'Skills',
 
     'footer-contact' : 'Contato',
-    'footer-socialmedia' : "Redes sociais"
+    'footer-socialmedia' : "Redes sociais",
+
+    'header-profile' : 'Perfil',
+    'header-desc' : 'Desenvolvedor Java',
+
+    'profile-about-key': 'Sobre',
+    'profile-about-value' : 'Atualmente sou desenvolvedor PHP e faço consultoria para a SKY (Empresa que tem como principal produto canais de televisão por assinatura.) no desenvolvimento de aplicações interativas (Aplicações que são transmitidas via satélite junto com o pacote de canais do cliente, essas aplicações podendo ser de venda de pay-per-view, marketing de novos pacotes e etc.).<br><br>Fui estagiário na SKY por 2 anos onde me desenvolvi no front-end e desenvolvi varias ferramentas de automação em Java, e atualmente sou desenvolvedor na empresa Esferas Software tendo a SKY como principal cliente.<br><br>Eu venho me desenvolvendo em Java pelos últimos 3 anos, sempre fui apaixonado pela linguagem e atualmente estou procurando uma oportunidade para voltar a desenvolver Java.',
+
+    'profile-name-key' : 'Nome: ',
+    'profile-name-value' : 'Leonardo Pinheiro Lacerda',
+
+    'profile-age-key' : 'Idade: ',
+    'profile-age-value' : '23 Anos',
+
+    'profile-location-key' : 'Localização',
+    'profile-location-value' : 'São Paulo, SP - Brasil'
 }
