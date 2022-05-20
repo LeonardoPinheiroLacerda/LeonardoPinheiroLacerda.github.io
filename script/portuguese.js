c8567174b42a5868802a56959468cf39f97f6bee
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2022-05-19 21:36:54 -0300

    Correção de profile

--- script/portuguese.js
+++ script/portuguese.js
@@ -14,7 +14,7 @@ const portuguese = {
     'header-desc' : 'Desenvolvedor Java',
 
     'profile-about-key': 'Sobre',
-    'profile-about-value' : 'Atualmente sou desenvolvedor PHP e faço consultoria para a SKY (Empresa que tem como principal produto canais de televisão por assinatura.) no desenvolvimento de aplicações interativas (Aplicações que são transmitidas via satélite junto com o pacote de canais do cliente, essas aplicações podendo ser de venda de pay-per-view, marketing de novos pacotes e etc.).<br><br>Fui estagiário na SKY por 2 anos onde me desenvolvi no front-end e desenvolvi varias ferramentas de automação em Java, e atualmente sou desenvolvedor na empresa Esferas Software tendo a SKY como principal cliente.<br><br>Eu venho me desenvolvendo em Java pelos últimos 3 anos, sempre fui apaixonado pela linguagem e atualmente estou procurando uma oportunidade para voltar a desenvolver Java.',
+    'profile-about-value' : 'Atualmente sou desenvolvedor PHP e faço consultoria para a SKY (Empresa que tem como principal produto canais de televisão por assinatura.) no desenvolvimento de aplicações interativas (Aplicações que são transmitidas via satélite junto com o pacote de canais do cliente, essas aplicações podendo ser de venda de pay-per-view, marketing de novos pacotes e etc.).<br><br>Fui estagiário na SKY por 2 anos onde me desenvolvi no front-end e desenvolvi varias ferramentas de automação em Java, e atualmente sou desenvolvedor na empresa Esferas Software tendo a SKY como principal cliente.<br>.',
 
     'profile-name-key' : 'Nome: ',
     'profile-name-value' : 'Leonardo Pinheiro Lacerda',
